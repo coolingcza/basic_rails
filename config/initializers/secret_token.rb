c192c195c1ca398051a6c8eb9bfbb462a1d83daf
Crystal Cooling <cooling.cne@gmail.com>
2015-03-17 11:27:42 -0500

    initial commit w/db.yml changes

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BasicRailsApp::Application.config.secret_token = '15167ef25ff082a058b2080cec9f30104012137a3099e5ef31d758f3199067d914015be7b0ccd5f8c93a05a7d9e6b0cc19d09a38cf62491a6932367645ae7a5a'
